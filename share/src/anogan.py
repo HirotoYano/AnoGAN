6194527406c00efac969e1418b4ff3235ff4614e
Hiroto Yano <mi22a012@oecu.jp>
2022-07-07 14:46:00 +0900

    [fix] Organizing the Library

--- share/src/anogan.py
+++ share/src/anogan.py
@@ -1,13 +1,12 @@
-from __future__ import print_function
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Reshape, Dense, MaxPooling2D, Conv2D, Flatten
-from tensorflow.keras.layers import Conv2DTranspose, LeakyReLU
-from tensorflow.keras.layers import Activation
-from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import (
+        Input, Reshape, Dense, MaxPooling2D, Flatten,
+        Conv2DTranspose, LeakyReLU,
+        Activation,
+        BatchNormalization
+)
 from tensorflow.keras.optimizers import RMSprop
 from tensorflow.keras import backend as K
-# from tensorflow.keras import initializers
-# from tqdm import tqdm
 from keras.utils. generic_utils import Progbar
 
 # import tensorflow as tf
--- share/src/main.py
+++ share/src/main.py
@@ -1,4 +1,3 @@
-from __future__ import print_function
 from sklearn.manifold import TSNE
 
 import os
